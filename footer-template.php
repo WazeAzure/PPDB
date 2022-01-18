614ef5926f489262d0960132f409879d9360fadf
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2022-01-19 06:14:08 +0700

    Adding footer-template.php

---
+++ footer-template.php
@@ -0,0 +1,5 @@
+<footer class="container">
+    <div class="footer-container text-center text-lg-start">
+        <small>Copyright &copy; 2022 SMA PRADITA DIRGANTARA. All Rights Reserved</small>
+    </div>
+</footer>
