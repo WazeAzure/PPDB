054246a1ce02b15fc9cd549f9254060a8b862599
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2022-01-19 06:19:36 +0700

    Adding index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include 'head-template.php'?>
+    <link rel="stylesheet" tyoe="css/text" href="media/css/index.css">
+    </head>
+    <body>
+        <?php include 'header-template.php'?> 
+        <div class="body">
+            
+            <main class="container">
+                <div class="main container">
+                    <h2><b>Pengumuman PPDB 2022</b></h2>
+                    <h1><b>SMA Pradita Dirgantara</b></h1>
+                    <div class="row justify-content-center timer-app">
+                        <div class="timer-app-container">
+                            <table id="timer-table" class="table table-borderless table-responsive">
+                                <tr class="text-md">
+                                    <th>Hari</th> 
+                                    <th></th>
+                                    <th>Jam</th>
+                                    <th></th>
+                                    <th>Menit</th>
+                                    <th></th>
+                                    <th>Detik</th>   
+                                </tr>
+                                <tr class="text-lg">
+                                    <td id="timer-days" class="display-2">0</td> 
+                                    <td class="display-1">:</td>
+                                    <td id="timer-hours" class="display-2">0</td>
+                                    <td class="display-1">:</td>
+                                    <td id="timer-minutes" class="display-2">0</td>
+                                    <td class="display-1">:</td>
+                                    <td id="timer-seconds" class="display-2">0</td>   
+                                </tr>
+                            </table>
+                            <!-- 
+                            <div class="card">
+                                <p>Hari</p>
+                                <p id="timer-days">0</p>
+                            </div> 
+                            <div class="card">
+                                <p>Jam</p>
+                                <p id="timer-hours">0</p>
+                            </div>
+                            <div class="card">
+                                <p>Menit</p>
+                                <p id="timer-minutes">0</p>
+                            </div>
+                            <div class="card">
+                                <p>Detik</p>
+                                <p id="timer-seconds">0</p>
+                            </div>
+                            -->     
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <?php include 'footer-template.php'?>        
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="media/js/index.js"></script>
+    </body>
+</html>
