4028e935a972aa95924493d82086123452d7da7a
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2022-01-19 06:19:39 +0700

    Adding user_data.php

---
+++ user_data.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<?php
+    $user_nisn = 0;
+    $user_ttl_dd = 0;
+    $user_ttl_mm = 0;
+    $user_ttl_yy = 0;
+
+    if(isset($_POST['submit'])){
+        $user_nisn = $_POST['user_no'];
+        $user_ttl_dd = $_POST['user_ttl_dd'];
+        $user_ttl_mm = $_POST['user_ttl_mm'];
+        $user_ttl_yy = $_POST['user_ttl_yy'];
+    }
+    
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+    $databasename = "test";
+     
+    $conn = new mysqli($servername, $username, $password, $databasename);
+    
+    if($conn->connect_error){
+        die("Connection Failed".$conn->connect_error);
+    }
+    // echo "Connection Succeed";
+
+    $sql = "SELECT * FROM Daftar_Siswa_Lolos WHERE NISN={$user_nisn} AND TTL='{$user_ttl_yy}-{$user_ttl_mm}-{$user_ttl_dd}'";
+    $result = $conn->query($sql);
+
+    // debug function
+
+    function debug_to($output){
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
+
+    if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+        if($row['Status'] == 1){
+            include 'user_dashboard.php';
+            // echo $row['Status'];
+            // debug_to($row['Status']);
+        } else{
+            include 'user_dashboard_fail.php';
+        } 
+    }
+
+    $conn->close();
+?>
