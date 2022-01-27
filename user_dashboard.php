191e0ce27fdd1a6895dfcdcd6033d79e029cf849
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2022-01-27 14:49:21 +0700

    Adding user_dashboard.php

--- user_dashboard.php
+++ user_dashboard.php
@@ -14,21 +14,27 @@
                         <img id="logo-pradita" src="media/image/Logo-PD-1-300x297.png">
                     </div>
                     <div class="card-body">
-                        <img class="img-fluid" src="<?php echo "data:image/jpeg;base64,".base64_encode($row['Gambar'])?>"/>
-                        <p class="h3">NISN: <?php echo $row['NISN']?></p>
-                        <h3 class="display-3 card-title bold color-yellow"><?php echo $row['Nama']?></h3>
-                        <p class="card-text">Calon Siswa SMA Pradita Dirgantara TA 2022/2023</p>
-                        <div class="row">
-                            <p class="color-yellow h4">Tanggal Lahir</p>
-                            <p><?php echo $row['TTL'];?></p>
+                        <div class="card-body-left">
+                            <img class="img-fluid" src="<?php echo "data:image/jpeg;base64,".base64_encode($row['Gambar'])?>"/>
                         </div>
-                        <div class="row">
-                            <p class="color-yellow h4">SMP Asal</p>
-                            <p><?php echo $row['Sekolah'];?></p>
-                        </div> 
-                        <div class="row">
-                            <p class="color-yellow h4">Lanud Asal</p>
-                            <p><?php echo $row['Asal_Lanud'];?></p>
+                        <div class="card-body-right">
+                            <p class="h3 color-yellow">NISN: <?php echo $row['NISN']?></p>
+                            <h3 class="display-3 card-title bold"><?php echo $row['Nama']?></h3>
+                            <p class="card-text">Calon Siswa SMA Pradita Dirgantara TA 2022/2023</p>
+                            <div class="card-body-right-bottom">
+                                <div class="row">
+                                    <p class="color-yellow h4">Tanggal Lahir</p>
+                                    <p><?php echo $row['TTL'];?></p>
+                                </div>
+                                <div class="row">
+                                    <p class="color-yellow h4">SMP Asal</p>
+                                    <p><?php echo $row['Sekolah'];?></p>
+                                </div> 
+                                <div class="row">
+                                    <p class="color-yellow h4">Lanud Asal</p>
+                                    <p><?php echo $row['Asal_Lanud'];?></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer justify-content-center">
