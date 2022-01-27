62dc00cd20410a59b25e2fe8c789fd6ca0c2fe49
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2022-01-27 14:49:19 +0700

    Adding header-template.php

--- header-template.php
+++ header-template.php
@@ -1,7 +1,7 @@
 <header>
     <div class=" header container-fluid p-0">
         <nav class="justify-content-center container-fluid navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="https://sma.praditadirgantara.sch.id/"><img src="media/image/Asset1.png" class="img-fluid"></a>
+            <a class="navbar-brand" href="https://sma.praditadirgantara.sch.id/"><img src="media/image/Asset1(3).jpg" class="img-fluid"></a>
         </nav>
     </div>
 </header>
