a8beeedf79c326c62a680c19d6f4a82abcf26712
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2022-01-27 14:49:22 +0700

    Adding user_not_found.php

---
+++ user_not_found.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <?php include 'head-template.php'?>
+        <link rel="stylesheet" type="text/css" href="media/css/user_not_found.css">
+    </head>
+    <body>
+        <?php include 'header-template.php'?>
+        <main>
+            <h1 class="display 1"> 404 Not Found</h1>
+            <a href="/PPDB/login.php">Kembali ke Halaman Login</a>
+        </main> 
+        <?php include 'footer-template.php'?>
+    </body>
+</html>
